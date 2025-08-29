b207b191f32c834693a271468b1b84ea08054521
1wair <1wairesd.com.industries@gmail.com>
2025-08-29 15:22:45 +0300

    feat: add listener for player death event to send webhook notification

---
+++ src/main/java/com/wairesd/webhookaction/event/PlayerDeath.java
@@ -0,0 +1,17 @@
+package com.wairesd.webhookaction.event;
+
+import com.wairesd.webhookaction.discord.webhook.Send;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+
+public class PlayerDeath implements Listener {
+
+    @EventHandler
+    public void playerdeath (PlayerDeathEvent event) {
+        String playerName = event.getPlayer().getName();
+
+        Send send = new Send();
+        send.send("Игрок " + playerName + " умер");
+    }
+}
